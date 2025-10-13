e49b8aec85b4f5cdb7bee11de1f4538b894b6583
wangbeijing <2231101379@qq.com>
2025-10-13 23:47:49 +0800

    新增雅思备考计划

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,24 +1,8 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '前端',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-    items: [
-      // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端文章',
-        items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-        ],
-      },
-      {
-        text: '学习笔记',
-        items: [
-          { text: '《JavaScript教程》', link: '/note/javascript/' },
-          { text: '《JavaScript高级程序设计》', link: '/note/js/' }
-        ],
-      },
-    ],
+    text: '备考计划',
+    link: '/IELTSPlan/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   },
   {
     text: '页面',
