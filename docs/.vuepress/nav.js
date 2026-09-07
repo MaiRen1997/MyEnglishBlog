64c0ea171866f2d6a84c7b2d613837e3387ae351
MaiRen <2231101379@qq.com>
2026-09-07 08:54:05 +0800

    修改笔记链接

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -2,7 +2,11 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: '备考计划',
-    link: '/IELTSPlan/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/englishLife/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      { text: '备考计划', link: '/IELTSPlan/' },
+      { text: '学习计划', link: '/basicStudyPlan/' },
+    ],
   },
   {
     text: '读书',
